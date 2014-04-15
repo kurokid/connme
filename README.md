170c219055cdf7ba2d06339102566939ad917d0a
Ikhwan Setiawan <kuro.kid@gmail.com>
2014-04-16 01:24:32 +0700

    Rebuild project

---
+++ README.md
@@ -0,0 +1,19 @@
+===============================
+connme
+===============================
+
+Client for Hostapd
+A simple application to facilitate the creation of the Wireless Access Point using hostapd.
+
+* Free software: GPL license
+
+Features
+--------
+* Create an AP (Access Point).
+* Choose one of the following encryptions: WPA, WPA2, WPA/WPA2, Open (no encryption).
+* List all connected client.
+* Stop internet connection for each client.
+
+### TODO
+* Limit speed for each client
+* Add description and image based on client mac address
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='connme',
+    version='0.1.0',
+    description='Client untuk Hostapd',
+    long_description=readme + '\n\n' + history,
+    author='Ikhwan Setiawan',
+    author_email='kuro.kid@gmail.com',
+    url='https://github.com/kurokid/connme',
+    packages=[
+        'connme',
+    ],
+    package_dir={'connme': 'connme'},
+    include_package_data=True,
+    install_requires=[
+        'hostapd', 
+        'dnsmasq', 
+        'iptables', 
+        'iw',
+    ],
+    data_files=[
+        ('share/applications', ['data/connme.desktop']),
+        ('share/pixmaps', ['data/connme.png']),
+        ('/etc', ['data/connme.conf']),
+    ],
+    scripts = ['bin/connme', 'bin/tether'],
+    license="GPL",
+    zip_safe=False,
+    keywords='connme',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+)
