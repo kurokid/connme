b3bba605be62f13faaa37177551040c387c5711e
Ikhwan Setiawan <kuro.kid@gmail.com>
2014-04-16 14:11:04 +0700

    minor fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='connme',
-    version='0.1.0',
+    version='0.5',
     description='Client untuk Hostapd',
     long_description=readme + '\n\n' + history,
     author='Ikhwan Setiawan',
